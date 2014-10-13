f733b544f10d16a206d34b05c33b64ecbb0c3e6f
Olivier <olivier.simart@gmail.com>
2014-10-13 17:15:56 -0400

    Disabling login for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  before_action :require_login
+  # before_action :require_login
   before_action :current_user
 
   private
 
     def current_user
     	# session[:user_id] = User.includes(:authentications).first
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      if session[:user_id]
+        @current_user ||= User.find(session[:user_id]) #if session[:user_id]
+      else
+        @current_user = User.first
+      end
     end
     helper_method :current_user
 
