b17a629a0b80ab048a93952d27169f88f04dd2b5
jacktran <jack@nustechnology.com>
2014-11-24 08:29:22 +0700

    fix issue - FactoryGirl::DuplicateDefinitionError - User can only see what they're supposed to see

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -22,19 +22,13 @@ RSpec.describe SearchesController, :type => :controller do
     end
 
     it "load all events with search criteria" do
-      application = FactoryGirl.create :application, owner: current_user, members: [current_user]
-      events = create_events application
+      application_owner = FactoryGirl.create :application, owner: current_user, members: [current_user]
+      application_other = FactoryGirl.create :application, owner: FactoryGirl.create(:user)
+      create_events application_other
+      events = create_events application_owner
 
       get :show, id: @search.id
       expect(assigns(:events)).to match_array(events)
-    end
-
-    it "without events with search criteria" do
-      application = FactoryGirl.create :application, owner: FactoryGirl.create(:user)
-      events = create_events application
-
-      get :show, id: @search.id
-      expect(assigns(:events)).not_to match_array(events)
     end
 
   end
