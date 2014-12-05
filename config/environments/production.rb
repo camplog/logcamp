f6ccc375beb95e32016a65c67d91f52f5eff5cf5
Olivier <olivier.simart@gmail.com>
2014-12-04 19:56:46 -0500

    Added env. domain for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   config.action_mailer.smtp_settings = {
     :address              => ENV['SMTP_HOST'],
