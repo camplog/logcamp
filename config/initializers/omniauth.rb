b51ae0798af277960bce6caab1259a1939097cd7
Olivier <olivier.simart@gmail.com>
2015-01-14 15:29:36 -0500

    Disabled local url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(:centralid,
            ENV['OAUTH2_CLIENT_ID'],
-           ENV['OAUTH2_CLIENT_SECRET'],
-           client_options: {
-             site: 'http://localhost:3029'
-           })
+           ENV['OAUTH2_CLIENT_SECRET'])
+           # client_options: {
+           #   site: 'http://localhost:3029'
+           # })
            # provider_ignores_state: true)
            # provider_ignores_state is a hack for CSRF issue. See details below
            # https://github.com/intridea/omniauth-oauth2/issues/58
