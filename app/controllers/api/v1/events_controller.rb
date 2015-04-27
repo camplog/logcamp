be52a1be51a73e6845183d0fbe709c774b8b0ea3
Olivier <olivier.simart@gmail.com>
2015-04-27 11:26:14 -0400

    Sorting events by date. Deliver notifications now

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -5,7 +5,7 @@ module Api
       # curl http://localhost:3023/api/events -H 'Authorization: Token token="111"'
       # curl http://localhost:3023/api/events?q=429 -H 'Authorization: Token token="111"'
       def index
-        @events = @current_application.events.order('created_at DESC').limit(20)
+        @events = @current_application.events.order('date DESC').limit(20)
         if params[:q].present?
           @events = @current_application.events.search_by_keyword(params[:q])
         end
@@ -23,8 +23,8 @@ module Api
         @event = @current_application.events.new(safe_params)
         if @event.save
           render @event.as_json, status: :created
-          #render json: @event, status: :created
-          EventMailer.notify_members(@event).deliver_later if @event.alert?
+          # render json: @event, status: :created
+          EventMailer.notify_members(@event).deliver_now if @event.alert?
         else
           render json: @event.errors, status: 422
           puts "**** #{@event.errors.full_messages.to_sentence} ****"
@@ -32,6 +32,7 @@ module Api
       end
 
       private
+
         def safe_params
           params.require(:event).permit(:status, :message, :alert, :date).tap do |white_listed|
             white_listed[:metadata] = params[:event][:metadata]
